f5aa1552dff75f2df8bcc2d316288784a72c91b7
Chris Camacho <cacamacho91@gmail.com>
2019-02-04 13:28:11 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+artists = [
+  {name: "Dr. Dre"},
+  {name: "Eminem"},
+  {name: "Xibit"},
+  {name: "50 Cent"}
+]
+Artist.create(artists)
+
+songs = [
+  {title: "Rappy Rap Song", artist: Artist.first},
+  {title: "Hipdy-Hip-Hop-Song", artist: Artist.first},
+  {title: "Song 3", artist: Artist.second},
+  {title: "Rapy Song boi", artist: Artist.third}
+]
+Song.create(songs)
