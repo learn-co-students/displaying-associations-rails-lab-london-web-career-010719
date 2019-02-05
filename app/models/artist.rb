98568e49b85dacc20cd50e80b451f9c90a1f5ba4
jchaselubitz <jchaselubitz@gmail.com>
2019-02-05 10:06:03 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,18 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def artist_songs
+        artist = Artist.find(self.id)
+        artist.songs.each do |song|
+            song
+        end
+    end
+
+    
+    def song_count
+        artist_songs.count
+    end
+
+
+
 end
